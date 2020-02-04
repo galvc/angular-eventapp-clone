0ecaebb3296f6bec75ae907f2c94248af8ee59cf
galvc <chelseatheg@gmail.com>
2020-02-04 13:26:38 -0800

    add event details html, fix layout

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from "@angular/router";
+import { e}
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
   styleUrls: ['./event-details.component.css']
 })
 export class EventDetailsComponent implements OnInit {
+  id: string;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get("eventId");
+    })
+    console.log(this.id)
   }
 
 }
