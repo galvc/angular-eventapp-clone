0b3ea793f3056f324b15ee586c8ea4c0bc27cfcf
galvc <chelseatheg@gmail.com>
2020-02-06 12:16:59 -0800

    display add form

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,33 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { EventsService } from '../events.service';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { EventsService } from "../events.service";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  selector: "app-admin",
+  templateUrl: "./admin.component.html",
+  styleUrls: ["./admin.component.css"]
 })
 export class AdminComponent implements OnInit {
-  constructor(private eventsService: EventsService) { }
+  constructor(private eventsService: EventsService) {}
   @Input() events;
-
+  addFormShow: boolean = false;
   ngOnInit() {
     this.getEvents();
   }
 
   getEvents() {
-    this.eventsService.getAllEvents()
-    .subscribe( e => this.events = e);
+    this.eventsService.getAllEvents().subscribe(e => {
+      this.events = e;
+      this.getEvents();
+    });
   }
 
-//i emitted an event
+  //i emitted an event
   deleteEvent(event) {
-    console.log('trying to delete')
-    console.log(event);
-    this.eventsService.deleteEvent(event.id).subscribe(
-      e => {alert(`${name} has been deleted`)
-      this.getEvents();}
-    )
+    this.eventsService.deleteEvent(event.id).subscribe(e => {
+      alert(`${name} has been deleted`);
+      this.getEvents();
+    });
   }
 
+  displayAddForm() {
+    this.addFormShow = !this.addFormShow;
+  }
 }
